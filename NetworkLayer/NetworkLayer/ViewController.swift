2bac1798fe7752ee0dc883787754d0ac89f0e708
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2019-01-29 13:58:56 +0700

    Create project

---
+++ NetworkLayer/NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by MACOS on 1/29/19.
+//  Copyright © 2019 MACOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
